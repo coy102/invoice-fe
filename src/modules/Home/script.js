ac3c9147d7d917eaed3179c59a14cefd993f274b
coy102 <fajarsep12@gmail.com>
2022-08-23 16:43:53 +0700

    Initial commit

---
+++ src/modules/Home/script.js
@@ -0,0 +1,76 @@
+import {
+  CBox,
+  CButton,
+  CAvatarGroup,
+  CAvatar,
+  CAvatarBadge,
+  CModal,
+  CModalContent,
+  CModalOverlay,
+  CModalHeader,
+  CModalFooter,
+  CModalBody,
+  CModalCloseButton,
+  CIconButton,
+  CFlex,
+  CHeading,
+} from '@chakra-ui/vue'
+
+export default {
+  name: 'Home',
+  components: {
+    CBox,
+    CButton,
+    CAvatarGroup,
+    CAvatar,
+    CAvatarBadge,
+    CModal,
+    CModalContent,
+    CModalOverlay,
+    CModalHeader,
+    CModalFooter,
+    CModalBody,
+    CModalCloseButton,
+    CIconButton,
+    CFlex,
+    CHeading,
+  },
+  inject: ['$chakraColorMode', '$toggleColorMode'],
+  data() {
+    return {
+      showModal: false,
+      mainStyles: {
+        dark: {
+          bg: 'gray.700',
+          color: 'whiteAlpha.900',
+        },
+        light: {
+          bg: 'white',
+          color: 'gray.900',
+        },
+      },
+    }
+  },
+  computed: {
+    colorMode() {
+      return this.$chakraColorMode()
+    },
+    theme() {
+      return this.$chakraTheme()
+    },
+    toggleColorMode() {
+      return this.$toggleColorMode
+    },
+  },
+  methods: {
+    showToast() {
+      this.$toast({
+        title: 'Account created.',
+        description: "We've created your account for you.",
+        status: 'success',
+        duration: 10000,
+        isClosable: true,
+      })
+    },
+  },
+}
---
+++ src/pages/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="container">
+    <Home />
+  </div>
+</template>
+
+<script lang="js">
+import Home from '../modules/Home'
+
+export default {
+  name: 'IndexPage',
+  components: {
+    Home
+  },
+
+}
+</script>
